480ddcf4613e025ee2d9599cf189708f94a8d707
AnhPhamBigin <anh.pham@bigin.vn>
2019-03-10 09:54:23 +0700

    fix install

--- deploy/install-server.sh
+++ deploy/install-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ~
-wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.8_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/a/apt/apt_1.8.0_amd64.deb
 sudo dpkg -i apt_1.4_amd64.deb
 sudo apt-get supervisor -y
